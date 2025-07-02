d2c22be836c46b67df0f69035124b538255c8b66
bre30kra69cs <an.krasowsckij@gmail.com>
2025-07-03 02:04:16 +0300

    feat: add prettier

---
+++ prettier.config.js
@@ -0,0 +1,16 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+  printWidth: 100,
+  trailingComma: "all",
+  tabWidth: 2,
+  semi: true,
+  useTabs: false,
+  arrowParens: true,
+  trailingComma: true,
+  singleQuote: true,
+};
+
+export default config;
